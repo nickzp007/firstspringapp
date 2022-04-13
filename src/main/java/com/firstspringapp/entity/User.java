40efb3782d8170f9b9c0cef4f16e5d149efe056a
unknown <nikhil98patil@gmail.com>
2022-04-13 23:50:15 +0530

    Added firstSpringApp

---
+++ src/main/java/com/firstspringapp/entity/User.java
@@ -0,0 +1,23 @@
+package com.firstspringapp.entity;
+
+public class User {
+
+    private String firstname;
+    private String lastname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirtname(String firtname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
